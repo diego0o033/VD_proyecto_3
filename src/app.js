a25e1668b995aa0eadb7f136de7a5ea165dd51fa
Diego Vargas <105869790+diego0o033@users.noreply.github.com>
2024-06-08 13:24:04 +0000

    proyecto_3_terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 /* eslint-disable */
-import "bootstrap";
+/*import "bootstrap";*/
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+/*import "./assets/img/rigo-baby.jpg";*/
+/*import "./assets/img/4geeks.ico";*/
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+  document.querySelector("#las_excusas").innerHTML = generador_De_Excusa();
   console.log("Hello Rigo from the console!");
+};
+let generador_De_Excusa = () => {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class','when I was sleeping','while I was exercising','during my lunch','while I was praying'];
+
+  let whoidx = Math.floor(Math.random * who.length);
+  let actionidx = Math.floor(Math.random * action.length);
+  let whatidx = Math.floor(Math.random * what.length);
+  let whenidx = Math.floor(Math.random * when.length);
+
+  return who[whoidx] + ' ' + action[actionidx] + ' ' + what[whatidx] + ' ' + when[whenidx];
 };
